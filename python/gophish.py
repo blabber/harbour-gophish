1d3c5153397241a755b45707dcb49e1202c38059
Tobias Rehbein <tobias.rehbein@web.de>
2017-11-08 18:40:51 +0100

    Add context to errors

--- python/gophish.py
+++ python/gophish.py
@@ -20,11 +20,11 @@ def read_menu(url):
     An additional key url is provided for convenience. It contains the gopher
     URL locating the menu item.
     """
+    items = []
 
     try:
         r = gopher.request.request_from_url(url)
 
-        items = []
         for i in gopher.menu.read_menu(r):
             items.append({'type':i.type, 'user_name':i.user_name,
                 'selector':i.selector, 'host':i.host, 'port':i.port,
@@ -32,31 +32,44 @@ def read_menu(url):
 
         return(items)
     except:
-        send_gophish_error(sys.exc_info())
+        send_gophish_error(sys.exc_info(), url)
+    finally:
+        return items
 
 def read_text(url):
     """Reads a text item identifed by a gopher URL and returns the content.
 
     Line separators are replaces with the local systems native line separator.
     """
+    text = ''
+
     try:
         r = gopher.request.request_from_url(url)
-        return gopher.text.read_text(r)
+        text = gopher.text.read_text(r)
     except:
-        send_gophish_error(sys.exc_info())
+        send_gophish_error(sys.exc_info(), url)
+    finally:
+        return text
 
-def send_gophish_error(ex):
+def send_gophish_error(ex, info=''):
     """Formats exception info in a string and sends it via the
     'gophishError' signal.
 
     ex is an exception info tuple as returned by sys.exc_info().
 
+    Additional information to be displayed in the UI can be passed
+    as info.
+
     As a side effect, the exception and a traceback are printed
     on the console.
     """
     traceback.print_exception(*ex)
-    pyotherside.send('gophishError',
-        os.linesep.join(traceback.format_exception_only(ex[0], ex[1])))
+
+    e = traceback.format_exception_only(ex[0], ex[1])
+    if info != '':
+        e = [info, ''] + e
+
+    pyotherside.send('gophishError', os.linesep.join(e))
 
 def parse_url(url):
     """Parses a gopher URL and returns a dictionary.
