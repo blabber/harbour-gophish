c3c3a333d43dc7011e98e3fe921497723d3b904b
Tobias Rehbein <tobias.rehbein@web.de>
2017-11-15 21:06:02 +0100

    Try to guess the server encoding on a best effort basis
    [RFC 1436](https://www.ietf.org/rfc/rfc1436.txt) defines ASCII as the encoding
    used by the gopher protocol, but this seems to be outdated by now. Being a
    modern client, gophish supports two supersets of the ASCII encoding:
    
    * UTF-8 (preferred)
    * Latin-1
    
    When first contacting a server, gophish will send a UTF-8 encoded selector.
    This should be no problem, as long as the server is not using non-ASCII
    characters in selectors (which is a bad idea anyway).
    
    gophish tries to decode the data retrieved from the server using UTF-8 and if
    that fails, it falls back to Latin-1. Once gophish had to fall back to Latin-1,
    the server is marked as being Latin-1 encoded internally and all following
    communication, including the selectors, will be encoded in Latin-1.

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ Supported item types
   * traditional `GET` requests
   * `URL:` links
 
+Encoding
+--------
+
+[RFC 1436](https://www.ietf.org/rfc/rfc1436.txt) defines ASCII as the encoding
+used by the gopher protocol, but this seems to be outdated by now. Being a
+modern client, gophish supports two supersets of the ASCII encoding:
+
+* UTF-8 (preferred)
+* Latin-1
+
+When first contacting a server, gophish will send a UTF-8 encoded selector.
+This should be no problem, as long as the server is not using non-ASCII
+characters in selectors (which is a bad idea anyway).
+
+gophish tries to decode the data retrieved from the server using UTF-8 and if
+that fails, it falls back to Latin-1. Once gophish had to fall back to Latin-1,
+the server is marked as being Latin-1 encoded internally and all following
+communication, including the selectors, will be encoded in Latin-1.
+
--- python/gopher/request.py
+++ python/gopher/request.py
@@ -9,6 +9,8 @@ class Request:
     The request can be performed using the get_raw_data method.
     """
 
+    encodings = {}
+
     def __init__(self, type, host, port='70', selector=''):
         """Initialized a new Request instance."""
         self.type = type
@@ -23,26 +25,56 @@ class Request:
         The request will time out if no connection can be established in
         timeout seconds.
 
-        Selectors are send as UTF-8 encoded strings. Data is read as binary
-        data and then decoded into a UTF-8 string. If the decoding of the data
-        fails, it is interpreted as Latin-1.
+        Communication with the server is encoded in UTF-8. If the received
+        data can't be decoded using UTF-8, it is interpreted as Latin-1
+        encoded. If the dara can be decoded using Latin-1, all further
+        communication with the server will be encoded in Latin-1 (That implies,
+        that the first selector send to any server is always encoded as UTF-8).
         """
         b = b''
         with socket.create_connection((self.host, self.port), timeout) as s:
             s.settimeout(None)
             with s.makefile(mode='brw') as f:
-                f.write((self.selector+'\r\n').encode('utf-8'))
+                f.write((self.selector+'\r\n').encode(self.encoding()))
                 f.flush()
-                b =f.read()
+                b = f.read()
 
         try:
-            bs = str(b, encoding='utf-8')
-            print('encoding: utf-8')
+            bs = str(b, encoding=self.encoding())
             return bs
         except UnicodeDecodeError:
             bs = str(b, encoding='latin-1')
-            print('encoding: latin-1')
+            self.override_encoding('latin-1')
             return bs
+
+    def encoding(self):
+        """Returns the encoding used in communication with the server.
+
+        By default the communication is UTF-8 encoded, but the encoding can be
+        overriden, using the override_encoding method.
+        """
+        key = '{0}:{1}'.format(self.host, self.port)
+
+        if not key in Request.encodings:
+            print('Default encoding for {0}: utf-8'.format(key))
+            return 'utf-8'
+
+        e = Request.encodings[key]
+        print('Registered encoding for {0}: {1}'.format(key, e))
+        return e
+
+    def override_encoding(self, encoding):
+        """Overrides the encoding used in communication with the server.
+
+        All following communication with the server, by this or any other
+        Request instances, will use the new encoding.
+        """
+        old = self.encoding()
+        if old != encoding:
+            key = '{0}:{1}'.format(self.host, self.port)
+            Request.encodings[key] = encoding
+            print('Override encoding for {0}: {1} -> {2}'.format(
+                key, old, encoding))
 
     def __eq__(self, other):
         """Returns True if the other Request represents the same request."""
